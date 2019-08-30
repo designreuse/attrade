1f2b0e2f0fc866e78600a878a5ae9666b0a7721c
mgsviryd <mgsviryd@gmail.com>
2019-08-30 17:54:30 +0300

    FLYWAY: migrate db / hibernate.ddl-auto=validate https://www.youtube.com/watch?v=ArM7nCys4hY&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk&index=12

--- web/src/main/java/by/attrade/domain/Message.java
+++ web/src/main/java/by/attrade/domain/Message.java
@@ -23,7 +23,7 @@ public class Message {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer id;
+    private Long id;
     private String text;
     private String tag;
     @ManyToOne(fetch = FetchType.EAGER)
