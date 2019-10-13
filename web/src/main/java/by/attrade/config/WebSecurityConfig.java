ad00b64b8852ac7641e7aa1a5b066cdecf29c82c
mgsviryd <mgsviryd@gmail.com>
2019-10-13 12:49:29 +0300

    oAuth2 (DENIED): reason: cannot achieve /login, always redirect to /login/google

--- web/src/main/java/by/attrade/config/WebSecurityConfig.java
+++ web/src/main/java/by/attrade/config/WebSecurityConfig.java
@@ -1,15 +1,9 @@
 package by.attrade.config;
 
 import by.attrade.provider.AuthProvider;
-import by.attrade.service.CustomUserInfoTokenServices;
 import by.attrade.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.NestedConfigurationProperty;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -19,26 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.web.filter.CompositeFilter;
-
-import javax.servlet.Filter;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableOAuth2Sso
-@EnableOAuth2Client
-@EnableAuthorizationServer
 @Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
@@ -47,33 +25,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private PasswordEncoder passwordEncoder;
     @Autowired
     private AuthProvider authProvider;
-    @Autowired
-    OAuth2ClientContext oauth2ClientContext;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(8);
     }
-
-//    @Bean
-//    public PrincipalExtractor principalExtractor(UserService userService) {
-//        return map -> {
-//            String sub = (String) map.get("sub");
-//            User userFromDB = userService.findBySub(sub);
-//            User user = Optional.ofNullable(userFromDB).orElseGet(() ->
-//                    User.builder()
-//                            .username(String.valueOf(map.get("email")))
-//                            .password("1Aa".concat(UUID.randomUUID().toString()))
-//                            .sub(sub)
-//                            .email(String.valueOf(map.get("email")))
-//                            .active((Boolean) map.get("email_verified"))
-//                            .roles(Collections.singleton(Role.USER))
-//                            .build()
-//            );
-//            log.debug("Try save new user: {}",user);
-//            return userService.save(user);
-//        };
-//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -109,9 +65,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .ignoringAntMatchers("/message/**")
-
-                .and()
-                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
         ;
     }
 
@@ -120,68 +73,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userService)
                 .passwordEncoder(passwordEncoder);
         auth.authenticationProvider(authProvider);
-    }
-
-    private Filter ssoFilter() {
-        CompositeFilter filter = new CompositeFilter();
-        List<Filter> filters = new ArrayList<>();
-        filters.add(ssoFilter(google(), "/login/google"));
-        filters.add(ssoFilter(facebook(), "/login/facebook"));
-        filters.add(ssoFilter(github(), "/login/github"));
-        filter.setFilters(filters);
-        return filter;
-    }
-    private Filter ssoFilter(ClientResources client, String path) {
-        OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
-        OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
-        filter.setRestTemplate(template);
-        CustomUserInfoTokenServices tokenServices = new CustomUserInfoTokenServices(
-                client.getResource().getUserInfoUri(), client.getClient().getClientId());
-        tokenServices.setUserService(userService);
-        tokenServices.setRestTemplate(template);
-        filter.setTokenServices(tokenServices);
-        return filter;
-    }
-
-    @Bean
-    @ConfigurationProperties("google")
-    public ClientResources google() {
-        return new ClientResources();
-    }
-
-    @Bean
-    @ConfigurationProperties("github")
-    public ClientResources github() {
-        return new ClientResources();
-    }
-
-    @Bean
-    @ConfigurationProperties("facebook")
-    public ClientResources facebook() {
-        return new ClientResources();
-    }
-
-    @Bean
-    public FilterRegistrationBean<OAuth2ClientContextFilter> oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
-        FilterRegistrationBean<OAuth2ClientContextFilter> registration = new FilterRegistrationBean<OAuth2ClientContextFilter>();
-        registration.setFilter(filter);
-        registration.setOrder(-100);
-        return registration;
-    }
-
-}
-class ClientResources {
-    @NestedConfigurationProperty
-    private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
-
-    @NestedConfigurationProperty
-    private ResourceServerProperties resource = new ResourceServerProperties();
-
-    public AuthorizationCodeResourceDetails getClient() {
-        return client;
-    }
-
-    public ResourceServerProperties getResource() {
-        return resource;
     }
 }
