dc06f73595689eda0712aaf6b6cb037ea02a83f1
mgsviryd <mgsviryd@gmail.com>
2019-10-19 18:22:06 +0300

    Turbolinks: add Interceptor which add in all headers "Turbolinks-Location" - cutting headers is prevented where there is redirect

--- web/src/main/java/by/attrade/config/MvcConfig.java
+++ web/src/main/java/by/attrade/config/MvcConfig.java
@@ -1,9 +1,11 @@
 package by.attrade.config;
 
+import by.attrade.util.RedirectInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -18,6 +20,11 @@ public class MvcConfig implements WebMvcConfigurer {
     }
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RedirectInterceptor());
     }
 
     @Override
